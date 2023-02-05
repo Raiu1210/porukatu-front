defb7bb789c97f39f721e73aa330a51f39f3364a
Raiu1210 <baio1484@gmail.com>
2023-02-05 20:43:34 +0900

    move to map page

--- pages/index.vue
+++ pages/index.vue
@@ -8,16 +8,23 @@
 
     <v-row justify="center" align="center">
       <v-col cols="12" sm="10" md="10">
-        <v-card>
-          <v-img
-            src="/road.jpg"
-            class="white--text align-end"
-            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-            height="200px"
-          >
-            <v-card-title class="headline font-weight-bold">Touring spot</v-card-title>
-          </v-img>
-        </v-card>
+        <nuxt-link to="/map" tag="div" class="nl">
+          <v-card>
+            <v-img
+              src="/road.jpg"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title class="headline font-weight-bold">Touring spot</v-card-title>
+              <v-card-text class="white--text">
+                ツーリングに役立つ情報を地図にまとめ。広い駐車場、整備工場、グルメスポット
+              </v-card-text>
+            </v-img>
+
+
+          </v-card>
+        </nuxt-link>
       </v-col>
     </v-row>
   </div>
@@ -41,3 +48,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .nl {
+    cursor: pointer;
+  }
+</style>
